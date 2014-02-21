f1fdce43b9d6b24ee4525d3af9d7802ded68e083
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-20 22:13:18 -0500

    added game screens

--- src/com/egyptianratscrew/view/TitleView.java
+++ src/com/egyptianratscrew/view/TitleView.java
@@ -1,5 +1,6 @@
 package com.egyptianratscrew.view;
 
+import com.egyptianratscrew.activity.GameActivity;
 import com.egyptianratscrew.activity.R;
 
 import android.content.Context;
@@ -17,9 +18,13 @@ public class TitleView extends View {
 
 	private Bitmap titleGraphic;
 	private Bitmap playButtonUp;
+	private Bitmap optionButtonUp;
 	private Bitmap playButtonDown;
+	private Bitmap optionButtonDown;
+	
 	
 	private boolean playButtonPressed;
+	private boolean optionButtonPressed;
 	
 	private Context myContext;
 	
@@ -31,8 +36,10 @@ public class TitleView extends View {
 		myContext = context;
 		
 		titleGraphic = BitmapFactory.decodeResource(getResources(), R.drawable.title_graphic);
-		//playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
-		//playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
+		playButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_up);
+		optionButtonUp = BitmapFactory.decodeResource(getResources(), R.drawable.option_button_up);
+		playButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.play_button_down);
+		optionButtonDown = BitmapFactory.decodeResource(getResources(), R.drawable.option_button_down);
 	}
 	@Override
 	public void onSizeChanged (int w, int h, int oldw, int oldh) {
@@ -48,12 +55,22 @@ public class TitleView extends View {
 	@Override
 	protected void onDraw(Canvas canvas){
 	canvas.drawBitmap(titleGraphic, (screenW - titleGraphic.getWidth())/2, 0,null); // (0,0,null? writes title
-	if (playButtonPressed) {
-	canvas.drawBitmap(playButtonDown, (screenW-playButtonDown.getWidth())/2, (int) (screenH*0.7), null);
+	if (playButtonPressed) 
+	{
+		canvas.drawBitmap(playButtonDown, (screenW-playButtonDown.getWidth())/2, (int) (screenH*0.7), null);
+		canvas.drawBitmap(optionButtonUp, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7 + playButtonUp.getHeight()), null);
+	}
+	else if (optionButtonPressed)
+	{
+		canvas.drawBitmap(optionButtonDown, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7 + playButtonUp.getHeight()), null);
+		canvas.drawBitmap(playButtonUp, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7), null);
 	}
 	else
-	canvas.drawBitmap(playButtonUp, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7), null);
-
+	{
+		canvas.drawBitmap(playButtonUp, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7), null);
+		canvas.drawBitmap(optionButtonUp, (screenW-playButtonUp.getWidth())/2, (int) (screenH*0.7 + playButtonUp.getHeight()), null);
+	
+	}
 	 
 	}
 	public boolean onTouchEvent(MotionEvent event )
@@ -69,16 +86,28 @@ public class TitleView extends View {
 			{ 
 				playButtonPressed = true; 
 			}
+			else if (X > (screenW-playButtonUp.getWidth())/2 && X < ((screenW-playButtonUp.getWidth())/2) + playButtonUp.getWidth() 
+					&& Y > (int)(screenH*0.7 + playButtonUp.getHeight()) && Y < (int)(screenH*0.7) +playButtonUp.getHeight()*2)
+			{
+				optionButtonPressed = true;
+			}
 			break;
 		case MotionEvent.ACTION_MOVE:
 			break;
 		case MotionEvent.ACTION_UP:
 			if (playButtonPressed)
 			{
-				//Intent gameIntent = new Intent(myContext, GameActivity.class);
+				Intent gameIntent = new Intent(myContext, GameActivity.class);
+				myContext.startActivity(gameIntent);
+			}
+			else if (optionButtonPressed)
+			{
+				//Intent gameIntent = new Intent(myContext, OptionActivity.class);
 				//myContext.startActivity(gameIntent);
+				//For a later Assignment
 			}
 			playButtonPressed =false;
+			optionButtonPressed = false;
 			break;
 		
 	}
