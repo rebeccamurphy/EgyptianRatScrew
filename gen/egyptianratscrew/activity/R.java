d9c8e20ef429db540321b5a07b6b85a8ee33e65f
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-04-30 18:05:04 -0400

    adding buttons to option screen.

--- gen/egyptianratscrew/activity/R.java
+++ gen/egyptianratscrew/activity/R.java
@@ -83,10 +83,19 @@ public final class R {
     }
     public static final class id {
         public static final int GAME=0x7f090000;
-        public static final int action_settings=0x7f090004;
-        public static final int save_btn=0x7f090003;
-        public static final int seekBar1=0x7f090001;
-        public static final int spinnerRules=0x7f090002;
+        public static final int action_settings=0x7f09000d;
+        public static final int button1=0x7f090002;
+        public static final int save_btn=0x7f090001;
+        public static final int seekBar1=0x7f09000a;
+        public static final int seekBar2=0x7f090007;
+        public static final int spinnerRules=0x7f090006;
+        public static final int textView1=0x7f090005;
+        public static final int textView2=0x7f090009;
+        public static final int textView3=0x7f090008;
+        public static final int textView4=0x7f09000c;
+        public static final int textView5=0x7f09000b;
+        public static final int toggleButton1=0x7f090004;
+        public static final int toggleButton2=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
